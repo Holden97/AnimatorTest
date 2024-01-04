68d7996b2e3c5c232442bbad1c9489700b51ffdd
Gyb <479276132@qq.com>
2024-01-04 23:28:12 +0800

    [add]添加行走动画

---
+++ Assets/Scripts/MovingController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AnimatorTest
+{
+    public class MovingController : MonoBehaviour
+    {
+        public Animator animator;
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                animator.SetBool("isMoving", true);
+            }
+            else
+            {
+                animator.SetBool("isMoving", false);
+            }
+        }
+    }
+}
